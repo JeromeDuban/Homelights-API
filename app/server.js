d2b223a7eb5b56bc3b2dd5f6abfaa5d2a0377ba3
jejefcgb <jduban@enseirb-matmeca.fr>
2018-10-01 00:05:01 +0200

    config methods added

--- app/server.js
+++ app/server.js
@@ -5,6 +5,22 @@ var express     = require('express');
 var app         = express();
 var bodyParser  = require('body-parser');
 var morgan      = require('morgan');
+var fs 			= require('fs');
+
+
+var config  	=  {name:"defaultName"};
+
+var file_path 	= 'data.json';
+ 
+if (fs.existsSync(file_path)){
+	config = JSON.parse(fs.readFileSync(file_path, "utf8"));
+	console.log("Fichier de configuration chargé");	
+}else{
+	console.log("Aucun fichier de configuration.");
+	console.log("Création du fichier de configuration...");
+	save();
+}
+
 
 // =======================
 // configuration =========
@@ -36,19 +52,56 @@ apiRoutes.get('/', function(req, res) {
 	return res.json({ message: 'Welcome to the coolest API on earth!' });
 });
 
-apiRoutes.get('/status/:ip',function(req,res){
-	var ip = req.params.ip;
-	if (ip){
-		var hosts = [ip];
-		hosts.forEach(function(host){
-		    ping.sys.probe(host, function(isAlive){
-		        return res.json({ success: true, status: isAlive });    
-		    });
-		});
-	}else {
-		return res.json({ success: false, message: "No ip provided" });    
+apiRoutes.get('/config/', function (req,res){
+	return res.send(config);
+});
+
+apiRoutes.put('/config/:name',function(req,res){
+	var name = req.params.name;
+
+	if (name){
+		config.name = name;
+		save();
+		return res.json({ success: true, message: "Serveur renommé avec succès." }); 
+
+	}else{
+		return res.status(400).json({message :"No name provided."});
 	}
 
+});
+
+apiRoutes.put('/config/red/:r/green/:g/blue/:b',function(req, res){
+	var r = req.params.r;
+	var g = req.params.g;
+	var b = req.params.b;
+
+	if(r && g && b){
+
+		var colors = {
+			red : r,
+			green : g, 
+			blue : b,
+		};
+
+		config["colors"] = colors;
+		save();
+		return res.send(true); //FIXME
+
+	}else{
+		return res.status(400).json({message : "GPIO manquant."});
+	}
+});
+
+
+apiRoutes.get('/lights/:r/:g/:b',function (req, res){
+	console.log(config);
+	if (!config.colors || !config.colors.red || !config.colors.green || !config.colors.blue){
+		// TODO SEND COMMAND
+		return res.status(500).json("Problème de configuration, un des GPIOS est mal renseigné.");
+	}else{
+
+		return res.json({sucess : true});
+	}
 });
 
 // apply the routes to our application with the prefix /api
@@ -59,3 +112,17 @@ app.use('/api', apiRoutes)
 // =======================
 app.listen(port);
 console.log('Magic happens at http://localhost:' + port);
+
+
+function save(){
+	fs.writeFileSync(file_path,JSON.stringify(config));
+}
+
+
+
+
+
+
+
+
+
